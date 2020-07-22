df75ba81f49c5fa48c2cc7760b7f299f0da6856b
Saida Davis <saihdavis@gmail.com>
2020-07-22 05:31:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,13 @@
 class Shoe
   
-  attr_accessor :color, :size, :material
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
   def initialize(brand)
     @brand = brand
   end
   
+
   
   
   
